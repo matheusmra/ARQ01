ff2d1fa66b273a729a6eff6d85d4173be9d1fdea
Matheus de Almeida <69125506+matheusmra@users.noreply.github.com>
2025-06-17 17:24:35 -0300

    r02

---
+++ Provas/R02/02b.v
@@ -0,0 +1,24 @@
+module SoP (
+    input A, B, C,
+    output F
+);
+    assign F = (~A & ~B & ~C) |
+               (~A & B  & ~C) |
+               ( A & ~B & ~C) |
+               ( A & ~B &  C);
+endmodule
+
+module test_SoP;
+    reg A, B, C;
+    wire F;
+
+    SoP uut (.A(A), .B(B), .C(C), .F(F));
+
+    initial begin
+        $display("A B C | F");
+        for (integer i = 0; i < 8; i = i + 1) begin
+            {A, B, C} = i;
+            #1 $display("%b %b %b | %b", A, B, C, F);
+        end
+    end
+endmodule
